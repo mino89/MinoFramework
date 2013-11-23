cb2afa1fecc4e7add75da799570f355821252acc
mino89 <pierdomenicoreitano@gmail.com>
2013-11-23 03:09:59 +0100

    test of navigation for mobile

--- js/minoFramework0.0.1.js
+++ js/minoFramework0.0.1.js
@@ -1,3 +1,8 @@
 //this is a js file
 $(document).ready(function(){
+        $('.js-mobile-menu').click(function(){
+            
+            $('.js-mobile-navigation').toggleClass('opened');
+            
+    });    
 });
