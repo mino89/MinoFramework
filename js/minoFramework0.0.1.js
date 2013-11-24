a017c4d414e91c13a61a4eb18be8972b50d6692e
mino89 <pierdomenicoreitano@gmail.com>
2013-11-24 21:05:50 +0100

    update code highlight

--- js/minoFramework0.0.1.js
+++ js/minoFramework0.0.1.js
@@ -1,9 +1,26 @@
 //this is a js file
+
+
+
 $(document).ready(function(){
         $('.js-mobile-menu').click(function(){
             
             $('.js-mobile-navigation').toggleClass('opened');
             
     });
+
+$('.highlighter').each(function() {
+    
+  $(this).text($(this).html());
+  $(this).text().replace('<', '&')
+  $(this).text().replace(/\t/g, '&nbsp;')
+});
+    
+//var codeReader = $('.code-highlighter').text().replace(/\t/g, '&nbsp;');
+//$( '.code-highlighter' ).replaceWith( codeReader );
+
+
+
+
 
 });
