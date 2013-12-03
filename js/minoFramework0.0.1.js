e6651d707dcd4d2878a591db2a3cf2b4000102ce
mino89 <pierdomenicoreitano@gmail.com>
2013-12-03 22:00:48 +0100

    updating mobile navigation

--- js/minoFramework0.0.1.js
+++ js/minoFramework0.0.1.js
@@ -3,9 +3,27 @@
 
 
 $(document).ready(function(){
-        $('.js-mobile-menu').click(function(){
+    
+    
+    var selector = $('.js-mobile-navigation');
+    var height = selector.outerHeight();
+     
+     selector.css('margin-top', - (height + 100));
+    
+            $('.js-mobile-menu').click(function(){
             
-            $('.js-mobile-navigation').toggleClass('opened');
+            
+
+            if (selector.hasClass('opened')) {
+              
+                selector.removeClass('opened');
+                selector.css('margin-top', - (height + 100));
+            }else{
+                
+              selector.removeAttr('style');
+              selector.addClass('opened');
+            }
+
             
     });
 
