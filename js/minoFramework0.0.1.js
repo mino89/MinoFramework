c6a7f87d7a0192dcdddc94c398237efd04a18365
mino89 <pierdomenicoreitano@gmail.com>
2013-12-06 23:53:02 +0100

    fixing mobile navigation issue

--- js/minoFramework0.0.1.js
+++ js/minoFramework0.0.1.js
@@ -2,37 +2,36 @@
 
 
 
+
 $(document).ready(function(){
-    
-    
+    var mq = window.matchMedia( "(max-width: 800px)" );
+    var mqReturn = window.matchMedia( "(min-width: 800px)" );
     var selector = $('.js-mobile-navigation');
     var height = selector.outerHeight();
-     
-     selector.css('margin-top', - (height + 100));
     
-            $('.js-mobile-menu').click(function(){
+    if (mq.matches) {
+        
+        selector.css('margin-top', - (height + 100));    
+        $('.js-mobile-menu').click(function(){
             
-            
-
-            if (selector.hasClass('opened')) {
-              
-                selector.removeClass('opened');
-                selector.css('margin-top', - (height + 100));
-            }else{
-                
-              selector.removeAttr('style');
-              selector.addClass('opened');
-            }
-
-            
+                if (selector.hasClass('opened')) {
+                  
+                    selector.removeClass('opened');
+                    selector.css('margin-top', - (height + 100));
+                }else{
+                    
+                  selector.removeAttr('style');
+                  selector.addClass('opened');
+                }
+        });
+    }
+    
+    $(window).resize(function(){
+        console.log($(window).width());
+        if (mqReturn.matches) {
+            selector.removeAttr('style');
+        }else{
+            selector.css('margin-top', - (height + 100));
+        }
     });
-
-    
-//var codeReader = $('.code-highlighter').text().replace(/\t/g, '&nbsp;');
-//$( '.code-highlighter' ).replaceWith( codeReader );
-
-
-
-
-
 });
