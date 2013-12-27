4913ac8d93f4b1940c11665afdd8cb5b73ceebdc
mino89 <pierdomenicoreitano@gmail.com>
2013-12-28 00:33:40 +0100

    creating footer

--- js/minoFramework0.0.1.js
+++ js/minoFramework0.0.1.js
@@ -1,27 +1,32 @@
 //this is a js file
 
+
+
+var fbHeight = 0;
 var mq = window.matchMedia( "(max-width: 799px)" );
 var mqReturn = window.matchMedia( "(min-width: 800px)" );
 var selector = $('.mobile-navigation');
 var target = $('.side-navigation');
-var height = target.outerHeight();
-var fbHeight = $('.fb-like-box').outerHeight();
+var height = target.outerHeight() + fbHeight;
 
-console.log(fbHeight);
+
+
 if (mq.matches) { 
 
     
-    selector.css('margin-top', - (height));
-   
+selector.css('margin-top', - (height));
 
 }
 
 $(document).ready(function(){
+
+
     $('.js-mobile-menu').click(function(){
         
             if (selector.hasClass('opened')) {
-              
+                
                 selector.removeClass('opened').css('margin-top', - (height));
+
             }else{
               selector.addClass('add-animation').removeAttr('style').addClass('opened');
             }
@@ -32,10 +37,12 @@ $(document).ready(function(){
         if (mqReturn.matches) {
             selector.removeAttr('style');
             if (selector.hasClass('opened')) {
+            
                selector.removeClass('opened'); 
             }
         }else{
             selector.css('margin-top', - (height));
+
             if (selector.hasClass('opened')) {
                selector.removeClass('opened');
             }
